6895e32b1d3dd45857f8aad69d090014353fd21b
Namort <38209685+Namort@users.noreply.github.com>
2018-05-03 20:55:03 +0200

    Add files via upload

---
+++ Mur.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<form method="post" action="Publication.php">
+<table>
+<p>
+<tr>
+ <td><label for="Pub">Publication :</label></td>
+      <td>  <input type="text" name="Pub" id="Pub"/></td>
+        <br />
+</tr>
+ <tr> <td> <input type="submit" name="Soumettre" value="Soumettre"></td></tr>  
+</p>
+</table>
+</form>
+</body>
+</html>
