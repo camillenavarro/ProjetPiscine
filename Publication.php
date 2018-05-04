f3a49ec60f0e8eded1f5326e41a323f6248c7ca6
Namort <38209685+Namort@users.noreply.github.com>
2018-05-04 21:27:02 +0200

    Add files via upload

--- Publication.php
+++ Publication.php
@@ -1,5 +1,19 @@
 <?php
+echo "Bonjour";
+if(isset($_POST['Afficher']))
+{
+  echo "Bonjour";
+ //Connexion à la BDD
+ $database = "piscine"; //Nom de la BDD
+ $db_handle = new mysqli("localhost", "root", "") or die ("Connexion au serveur impossible!"); //Vérification de la connexion au serveur
+ $db_found = $db_handle->select_db($database) or die ("Base de données introuvable!"); //Vérification que la BDD existe 
 
+ $SQL2 ="SELECT * FROM publication";
+ $result = $db_handle->query($SQL2);
+ //Fermeture de la connexion à la BDD
+ $db_handle->close();
+
+} 
 if(isset($_POST['Soumettre']))
 {
   echo"essai";
@@ -12,13 +26,14 @@ if(isset($_POST['Soumettre']))
    
   
       //Requête SQL poster une publication texte
-    $SQL = "INSERT INTO publication (`id_pub`, `id_user`, `id_media`, `type`,`texte`, `statut`, `acces`) VALUES (NULL, '1', NULL, '', '$recupText', NULL, 'publique') ";
+    $SQL = "INSERT INTO publication (`id_user`, `id_media`, `type`,`texte`, `statut`, `acces`) VALUES ('1', NULL, '', '$recupText', NULL, 'publique') ";
   $result = $db_handle->query($SQL);
 
 
         //Fermeture de la connexion à la BDD
         $db_handle->close();
 }
+
 
  ?>
 
