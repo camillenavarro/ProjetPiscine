272fad5b9aa0ed5e352d0274901b8419d26f1937
Fiona Chuet <fiona.chuet@gmail.com>
2018-05-05 02:44:17 +0200

    Publications texte par utilisateur + notif texte
    Tout sur la page accueil

--- Publication.php
+++ Publication.php
@@ -1,5 +1,5 @@
 <?php
-echo "Bonjour";
+//echo "Bonjour";
 if(isset($_POST['Afficher']))
 {
   echo "Bonjour";
@@ -16,22 +16,61 @@ if(isset($_POST['Afficher']))
 } 
 if(isset($_POST['Soumettre']))
 {
-  echo"essai";
- $recupText =  addslashes($_POST['Pub'] ) ;
- //Connexion à la BDD
+  //echo"essai";
+    if (isset($_POST['Pub'])) {
+        $recupText1 = nl2br(htmlspecialchars($_POST['Pub']));
+        $recupText=  addslashes($recupText1);
+    }
+    //Connexion à la BDD
       $database = "piscine"; //Nom de la BDD
       $db_handle = new mysqli("localhost", "root", "") or die ("Connexion au serveur impossible!"); //Vérification de la connexion au serveur
       $db_found = $db_handle->select_db($database) or die ("Base de données introuvable!"); //Vérification que la BDD existe 
    
-   
+   //Eviter que des ? apparaissent à la place des accents
+    $db_handle->query('SET NAMES utf8');
+    header('Content-Type: text/html; charset=utf-8');
+
+    //Récupération de l'id de l'utilisateur connecté
+    $SQL7 = "SELECT id_user FROM connexion";
+    $result7 = $db_handle->query($SQL7);
+    while ($db_field7 = $result7->fetch_assoc()) { 
+        $id_user = $db_field7["id_user"];
+    }
+    $type="texte";
+    
+    //Récupère les informations relatives à l'utilisateur connecté
+    $SQL8 = "SELECT * FROM utilisateur WHERE id_user = '$id_user'";
+    $result8 = $db_handle->query($SQL8);
+    while ($db_field8 = $result7->fetch_assoc()) { 
+        $nom = $db_field8["nom"];
+        $prenom = $db_field8["prenom"];
+    }
   
       //Requête SQL poster une publication texte
-    $SQL = "INSERT INTO publication (`id_user`, `id_media`, `type`,`texte`, `statut`, `acces`) VALUES ('1', NULL, '', '$recupText', NULL, 'publique') ";
+    $SQL = "INSERT INTO publication (`id_user`, `type`,`texte`, `statut`, `acces`) VALUES ('$id_user','$type', '$recupText', NULL, 'publique') ";
   $result = $db_handle->query($SQL);
+    
+    //On récupère l'id de la publication
+    $SQL3 = "SELECT id_pub FROM publication WHERE id_user = '$id_user' AND texte = '$recupText'" ;
+			$result3 = $db_handle->query($SQL3);
+			
+			while($db_field3 = $result3->fetch_assoc())
+			{
+				$id_pub = $db_field3['id_pub'];
+			}
 
-
+    //La publication crée une notification
+    if($type == "texte"){
+        //On créer une notification de type texte
+        $SQL4 = "INSERT INTO `notif_pub`(`id_user`, `id_pub`, `texte`) VALUES ('$id_user','$id_pub','Nouvelle publication')";
+        $db_handle->query($SQL4);
+    }
+    
         //Fermeture de la connexion à la BDD
         $db_handle->close();
+    
+    //Redirection
+    header("Location: accueil.php");
 }
 
 
--- accueil.php
+++ accueil.php
@@ -1,4 +1,20 @@
 <?php
+    //Variables 
+    $id_contacts = array();
+    $id_pub = array();
+    $texte_pub = array();
+    $detail_pub = array();
+    $nom_contact = array();
+    $prenom_contact = array();
+    $date_post = array();
+
+    $i = 0;
+    $j = 0;
+    $k = 0;
+    $m = 0;
+    $save = 0;
+    $n = 0;
+
     //Connexion à la BDD
     $database = "piscine"; //Nom de la BDD
     $db_handle = new mysqli("localhost", "root", "") or die ("Connexion au serveur impossible!"); //Vérification de la connexion au serveur
@@ -78,9 +94,73 @@
     else{
         $photo_fond = "background.png";
     }
+
+
+    //Informations liées aux notifications
+    //1) Récupérer tous les id des contacts dans la tables contact
+    $SQL7 = "SELECT * FROM contact WHERE id_user='$id_user'";
+    $result7 = $db_handle->query($SQL7);
+    
+    //Récupération des résultats
+    while ($db_field7 = $result7->fetch_assoc()) {
+        $id_contacts[$i] = $db_field7['id_user_contact'] ;
+        $i ++ ;  
+    }
+
+    //2) Récupérer les informations des notifications de publication
+    for($i = 0 ; $i < sizeof($id_contacts) ; $i++)
+	{
+        //On regarde déjà si l'utilisateur a égénré des notifications
+        $SQL11 = "SELECT COUNT(id_user) FROM notif_pub WHERE id_user='$id_contacts[$i]'";
+        $result11 = $db_handle->query($SQL11);
+            
+        while ($db_field11 = $result11->fetch_assoc()) {
+            $nombre = $db_field11['COUNT(id_user)'] ;
+        }
+
+        //Si il y a au moins un résultat = une notification associée à l'id
+        if($nombre > 0){
+                $SQL8 = "SELECT * FROM notif_pub WHERE id_user='$id_contacts[$i]'";
+                $result8 = $db_handle->query($SQL8);
+
+                //Récupération des résultats
+                while ($db_field8 = $result8->fetch_assoc()) {
+                    $id_pub[$j] = $db_field8['id_pub'] ;
+                    $texte_pub[$j] = $db_field8['texte'] ;
+                    
+                    //On récupère les noms et prénoms de l'auteur associés aux publications
+                    $SQL9 = "SELECT * FROM utilisateur WHERE id_user='$id_contacts[$i]'";
+                    $result9 = $db_handle->query($SQL9);
+
+                    //Récupération des résultats
+                    while ($db_field9 = $result9->fetch_assoc()) {
+                        $nom_contact[$j] = $db_field9['nom'] ;
+                        $prenom_contact[$j] = $db_field9['prenom'] ;
+                    }
+                    
+                     for($n = 0 ; $n < sizeof($id_pub) ; $n++)
+	               {
+                        $SQL10 = "SELECT * FROM publication WHERE id_pub='$id_pub[$n]'";
+                        $result10 = $db_handle->query($SQL10);
+
+                        //Récupération des résultats
+                        while ($db_field10 = $result10->fetch_assoc()) {
+                            $detail_pub[$j] = $db_field10['texte'] ;
+                            $date_post[$j] = $db_field10['date_post'];
+                        }
+                    }
+
+                    $j++;
+                }
+
+
+           
+        }    
+    }
+        
     //Libération des résultats
     $result->free();
-        
+
     //Fermeture de la connexion à la BDD
     $db_handle->close();
 ?> 
@@ -119,7 +199,30 @@
                 <!-- Renseignements supplémentaires -->
             </div>
             
-            <div id="acceuil_droite">
+            <div id="accueil_droite">
+                
+                <!-- Publication -->
+                <form action="publication.php" method="post">
+                <h3>Ecrire une publication</h3>
+                <p><textarea name="Pub" cols="40" rows="5" value="Votre texte..."></textarea></p>
+                <input type="submit" value="Envoyer" name="Soumettre">
+                </form>
+                
+                <!-- Les notifications -->
+                <div id="accueil_notif">
+                    <h3>Mes notifications</h3>
+                    <!-- On affiche les notifications par contact -->
+                    <!-- Ouverture de la boucle for -->
+                    <?php for($n = sizeof($id_contacts) ; $n > 0  ; $n--) { ?>
+                        <h4><?php echo $prenom_contact[$n]; ?> <?php echo $nom_contact[$n]; ?></h4>
+                    
+                    <?php for($i = sizeof($id_pub)-1 ; $i > -1  ; $i--) { ?>
+                    <p><?php if(($prenom_contact[$n] == $prenom_contact[$i])and ($nom_contact[$n] == $nom_contact[$i])){echo $texte_pub[$i]; ?> de <?php echo $prenom_contact[$i]; ?> <?php echo $nom_contact[$i]; ?> ! <br>  Postée le <?php echo $date_post[$i]; ?> <br> Détails:  <?php echo $detail_pub[$i];} ?></p>
+                    <!-- Fermeture de la boucle for -->
+                    <?php }} ?>
+                </div>
+                
+                <!-- Les demandes d'amis -->
             <form action = "reception_demande_front.php" method = "post">
 				<input type = "submit" value = "Voir les demandes d'amis">
 			</form>
